43d318d05a0dead3362ac59ae177066c8faaf2cd
wenwei63029869 <wwen1030@gmail.com>
2016-08-22 19:34:11 -0700

    fixed bugs in getElementsByClassname

--- others/javascript/getElementByClassname.js
+++ others/javascript/getElementByClassname.js
@@ -1,24 +1,21 @@
-var findMatchElement = function(node, classname) {
-  var result = []
+var findMatchElement = function(node, className) {
+  var result = [];
   var childElements = node.childNodes;
-  if (node.classList && node.classList.contains(classname)){
+  if (node.classList && node.classList.contains(className)){
     result.push(node);
-  };
+  }
   if (childElements) {
     for (var i = 0; i < childElements.length; i++) {
-      var newNode = childElements[i]
-      if (newNode.classList && newNode.classList.contains(classname)) {
-          result.push(newNode);
-      };
-      result = result.concat(findMatchElement(newNode));
-    };
-  };
+      var newNode = childElements[i];
+      result = result.concat(findMatchElement(newNode, className));
+    }
+  }
   return result;
 };
 
-var getElementByClassname = function(classname) {
+var getElementByClassname = function(className) {
   var result = [];
   var node = document.body;
-  result = result.concat(findMatchElement(node, classname));
-  console.log(result[0]);
+  result = result.concat(findMatchElement(node, className));
+  return result;
 };
